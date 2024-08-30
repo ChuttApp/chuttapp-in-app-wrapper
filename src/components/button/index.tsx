e434d352b3633de5f83a5894d9e89e162746883d
owusuappiah <charliemacarth@gmail.com>
2024-08-30 16:15:14 +0000

    dfdf

---
+++ src/components/button/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ButtonWrapper } from './styled'
+
+import { ButtonProps as BP, Spin } from 'antd';
+
+interface ButtonProps extends BP {
+  rounded?: boolean;
+  isLoading?: boolean;
+}
+
+export function Button({ disabled, isLoading, ...props }: ButtonProps) {
+  if (isLoading) disabled = true;
+  return (
+    <ButtonWrapper {...props} />
+  )
+}
+
+export function IconButton({ rounded, className, ...props }: ButtonProps) {
+  return (
+    <ButtonWrapper {...props} shape="circle" />
+  )
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
 export const isEmpty = (data: any) => {
-    return typeof data === "undefined" || data == null || data === "";
-  };
-  
+  return typeof data === "undefined" || data == null || data === "";
+};
+
+export const appendClass = (...classNames: Array<string | undefined>) => {
+  return classNames?.join(" ");
+};
