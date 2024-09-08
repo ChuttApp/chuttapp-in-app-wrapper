fd0883a4bf4dd37657fc071645e57a6c7193fba5
owusuappiah <charliemacarth@gmail.com>
2024-09-08 16:02:04 +0000

    mn

--- src/features/shoes/ShoeDetailsScreen.tsx
+++ src/features/shoes/ShoeDetailsScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import QRCode from "react-qr-code";
 
 export function ShoeDetailsScreen() {
-  const scandata = useScannerData(InAppService.ScannerActions.OPEN_SCANNER);
+  useScannerData(InAppService.ScannerActions.OPEN_SCANNER);
 
   return (
     <Screen
